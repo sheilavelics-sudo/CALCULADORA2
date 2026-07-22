75a00277bb0c8b7566cc4b1ccb0a4a1aa1cc22e4
sheilavelics-sudo <sheilavelics@gmail.com>
2026-07-22 10:08:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+Console.WriteLine("Bienvenido a la calculadora :D");
+
+Console.WriteLine("Introduce el primer número.");
+string numero1 = Console.ReadLine();
+
+Console.WriteLine("Introduce el segundo número.");
+string numero2 = Console.ReadLine();
+
+int n1 = int.Parse(numero1);
+int n2 = int.Parse(numero2);
+
+Console.WriteLine("¿Qué operación quieres hacer?");
+Console.WriteLine("+ para sumar");
+Console.WriteLine("- para restar");
+Console.WriteLine("* para multiplicar");
+Console.WriteLine("/ para dividir");
+
+string operación = Console.ReadLine();
+
+if(operación == "+")
+{
+    Console.WriteLine("El resultado es:");
+    Console.WriteLine(n1 + n2);
+}
+else if (operación == "-")
+{
+    Console.WriteLine("El resultado es:");
+    Console.WriteLine(n1 - n2);
+}
+else if (operación == "*")
+{
+    Console.WriteLine("El resultado es:");
+    Console.WriteLine(n1 * n2);
+}
+else if (operación == "/")
+{
+    Console.WriteLine("El resultado es:");
+    Console.WriteLine(n1 / n2);
+}
+else
+{
+    Console.WriteLine("Operación no válida.");
+    return;
+}
