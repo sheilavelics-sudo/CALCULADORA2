a563c725108655de0b25de7b65917984a6cde2e9
sheilavelics-sudo <sheilavelics@gmail.com>
2026-07-24 11:39:21 +0200

    Add files via upload

---
+++ CALCULADORA PRUEBA 1/Program.cs
@@ -0,0 +1,67 @@
+﻿while(true)
+{
+Console.WriteLine();
+Console.WriteLine("====CALCULADORA====");
+Console.WriteLine("1. Sumar");
+Console.WriteLine("2. Restar");
+Console.WriteLine("3. Multiplicar");
+Console.WriteLine("4. Dividir");
+Console.WriteLine("5. Salir");
+Console.WriteLine();
+
+int opción = int.Parse(Console.ReadLine());
+
+if(opción == 1)
+    {
+        Console.WriteLine("Introduce el primer número: ");
+        int n1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Introduce el segundo número: ");
+        int n2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("El resultado es: ");
+        Console.WriteLine(n1 + n2);
+    }
+else if(opción == 2)
+    {
+        Console.WriteLine("Introduce el primer número: ");
+        int n1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Introduce el segundo número: ");
+        int n2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("El resultado es: ");
+        Console.WriteLine(n1 - n2);
+    }
+else if(opción == 3)
+    {
+        Console.WriteLine("Introduce el primer número: ");
+        int n1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Introduce el segundo número: ");
+        int n2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("El resultado es: ");
+        Console.WriteLine(n1 * n2);
+    }
+else if(opción == 4)
+    {
+        Console.WriteLine("Introduce el primer número: ");
+        int n1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Introduce el segundo número: ");
+        int n2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("El resultado es: ");
+        Console.WriteLine(n1 / n2);
+    }
+else if(opción == 5)
+    {
+        Console.WriteLine("¡Hasta luego Mari Carmen!");
+        break;
+    }
+else
+    {
+        Console.WriteLine("Opción no válida.");
+    }
+}
